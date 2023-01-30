f32985775c2a8538b5cd2b089b08231513d099a9
Tadaiji <40037269+Tadaiji@users.noreply.github.com>
2023-01-30 22:43:15 +0100

    current project

--- Assets/HandsScript.cs
+++ Assets/HandsScript.cs
@@ -1,34 +1,46 @@
-using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using System;
 
-// Using PID Controller so adding the PID formular to the hands
-// https://vazgriz.com/621/pid-controllers/ 
-public class PhysicsHands : MonoBehaviour
+public class HandsScript: MonoBehaviour
 {
+
+
     [SerializeField] private GameObject player;
-    
+
     //Stuff for the inspector
-    [Header("PID")]
-    [SerializeField] float frequency = 50f; 
-    [SerializeField] float damping = 1f; 
-    [SerializeField] Transform target; 
+    [Header("PID")] [SerializeField] float frequency = 50f;
+    [SerializeField] float damping = 1f;
+    [SerializeField] Transform target;
     [SerializeField] Rigidbody playerRigidbody;
 
-    [SerializeField] float rotFrequency = 100f; 
+    [SerializeField] float rotFrequency = 100f;
     [SerializeField] float rotDamping = 0.9f;
-    [Space]
-    
-    [Header("HookesLaw")]
-    [SerializeField] float climbForce = 500f;
+
+    [Space] [Header("HookesLaw")] [SerializeField]
+    float climbForce = 500f;
+
     [SerializeField] float climbDrag = 250f;
     private bool _isColliding = false;
-    
+
     private Vector3 _previousPosition;
-    
+
     private Rigidbody _rigidbody;
+
+    [Header("Go-Go Arms")] [SerializeField]
+    private Transform origin_transform;
     
+    [SerializeField] private float threshold; //D
+    [SerializeField] private float coefficient; //k
     
-    
+    //public Vector3 origin_position; //RR  //Hier wird einfach direkt auf Origin_transform.position zugegriffen
+    //public Vector3 virtual_position; //RV //Das wird dann zum GameObject Transform.position
+
+    public float lenght = 0;
+    public float distance = 0;
+
+    //ToDo: Schauen was das displacement der Hande durch das Go-Go verursacht in der berechnung der sprungkraft 
 
     void Start()
     {
@@ -37,24 +49,45 @@ public class PhysicsHands : MonoBehaviour
         transform.rotation = target.rotation;
 
         _rigidbody = GetComponent<Rigidbody>();
-        
+
         // Eliminates upper limit of the rotation of the hands
         _rigidbody.maxAngularVelocity = float.PositiveInfinity;
 
         _previousPosition = transform.position;
     }
-    
+
+    // Update is called once per frame
+    void Update()
+    {
+        distance = Vector3.Distance(transform.position, origin_transform.position);
+
+        if (distance > threshold)
+        {
+            //Zuerst length ausrechnen 
+            float m = origin_transform.position.magnitude;
+            lenght = m + coefficient * Mathf.Pow((m - coefficient), 2f);
+
+            //Dann length auf Vector multiplizieren
+            transform.position = target.forward * (lenght);
+            //transform.position = target.TransformDirection(target.forward) * (lenght);
+            //transform.position = transform.TransformVector(target.TransformDirection(target.forward) * (lenght));
+            //transform.position = target.position * (lenght);
+            //transform.Translate(target.forward * lenght);
+        }
+    }
+
+
     void FixedUpdate()
     {
         //Debug.Log(_rigidbody.position);
-        
+
         PIDMovement();
         PIDRotation();
-        if(_isColliding)
+        if (_isColliding)
         {
             HookesLaw();
         }
-        
+
         //Hier noch aktuell rotation auf richtige position und das dann zukünftig in die Rotation einbauen
     }
 
@@ -64,7 +97,7 @@ public class PhysicsHands : MonoBehaviour
         Vector3 displacementFromResting = transform.position - target.position;
         Vector3 force = displacementFromResting * climbForce;
         float drag = GetDrag();
-        
+
         playerRigidbody.AddForce(force, ForceMode.Acceleration);
         playerRigidbody.AddForce(drag * -playerRigidbody.velocity * climbDrag, ForceMode.Acceleration);
     }
@@ -72,42 +105,43 @@ public class PhysicsHands : MonoBehaviour
     float GetDrag()
     {
         Vector3 handVelocity = (target.localPosition - _previousPosition) / Time.fixedDeltaTime;
-        
+
         //Inverse, add small number to avoid devide by 0, the faster we move the less drag and the slower more drag
         float drag = 1 / (handVelocity.magnitude + 0.01f);
         drag = Math.Clamp(drag, 0.03f, 1f);
 
         _previousPosition = transform.position;
-        
+
         return drag;
     }
 
     void PIDMovement()
     {
         //target = to follow 
-        
+
         //p
         float kp = (6f * frequency) * (6f * frequency) * 0.25f;
         //d
         float kd = 4.5f * frequency * damping;
-        
+
         float g = 1 / (1 + kd * Time.fixedDeltaTime + kp * Time.fixedDeltaTime * Time.fixedDeltaTime);
         float ksg = kp * g;
         float kdg = (kd + kp * Time.fixedDeltaTime) * g;
-        
+
         //calculate how much force/acceleration to add to physics hand
         Vector3 force = (target.position - transform.position) * ksg +
                         (playerRigidbody.velocity - _rigidbody.velocity) * kdg;
 
         _rigidbody.AddForce(force, ForceMode.Acceleration);
     }
+
     void PIDRotation()
     {
         //p
         float kp = (6f * rotFrequency) * (6f * rotFrequency) * 0.25f;
         //d
         float kd = 4.5f * rotFrequency * rotDamping;
-        
+
         float g = 1 / (1 + kd * Time.fixedDeltaTime + kp * Time.fixedDeltaTime * Time.fixedDeltaTime);
         float ksg = kp * g;
         float kdg = (kd + kp * Time.fixedDeltaTime) * g;
@@ -127,6 +161,7 @@ public class PhysicsHands : MonoBehaviour
         {
             //q.x = q.x + 90 ;
         }
+
         q.ToAngleAxis(out float angle, out Vector3 axis);
         axis.Normalize();
         axis *= Mathf.Deg2Rad;
@@ -154,3 +189,4 @@ public class PhysicsHands : MonoBehaviour
         _isColliding = false;
     }
 }
+
--- Assets/Scripts/GoGoArms.cs
+++ Assets/Scripts/GoGoArms.cs
@@ -1,12 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.UI;
 using UnityEngine;
 
 public class GoGoArms : MonoBehaviour
 {
     [SerializeField] private Transform origin_transform;
-    
     [SerializeField] private GameObject target;
+    [SerializeField] private Transform controller_transform;
     
     [SerializeField] private float threshold; //D
     [SerializeField] private float coefficient; //k
@@ -14,11 +15,13 @@ public class GoGoArms : MonoBehaviour
     public float origin_magnitude;
     public Vector3 origin_position; //RR
     public Vector3 virtual_position; //RV
+    public Vector3 own_position;
+    public Vector3 controller_position;
+    
+    public float lenght = 0;
+    public float distance = 0;
 
-    private float lenght = 0;
-    
-    
-    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -31,22 +34,29 @@ public class GoGoArms : MonoBehaviour
         //TODO: Überlegen ob localPosition oder worldPosition, vielleicht auch gameObject an Controller als Anker hängen und schauen ob besser. 
         //Oder dieses Script in das Physics hands script einbauen, da das die hände eigentlich steuert
         
-        origin_position = origin_transform.localPosition;
-        //origin_position = origin_transform.position;
+        //origin_position = origin_transform.localPosition;
+        origin_position = origin_transform.position;
+        
+        own_position = gameObject.transform.position;
+        
+        distance = Vector3.Distance(own_position, origin_position);
+
         
         //TODO: Magnitude noch nicht optimal
-        origin_magnitude = origin_position.magnitude;
+        //origin_magnitude = origin_position.magnitude;
         
-        if (origin_magnitude > threshold)
+        if (distance > threshold)
         {
             //Zuerst length ausrechnen 
             lenght = origin_position.magnitude + coefficient*Mathf.Pow((origin_position.magnitude - coefficient), 2f);
             
             //Dann length auf Vector multiplizieren
-            virtual_position = origin_position * (lenght);
-            
-            target.transform.localPosition = virtual_position;
-            //target.transform.position = virtual_position;
+            //virtual_position = origin_position * (lenght);
+            controller_position = controller_transform.position;
+            virtual_position = controller_position * (lenght);
+
+            //target.transform.localPosition = virtual_position;
+            target.transform.position = virtual_position;
         }
         
     }
